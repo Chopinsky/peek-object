96d183689e9d169bab41620c0ed84a145f1738dd
Jacob Zuo <chopinsky@live.com>
2017-09-02 08:48:15 -0500

    initial commit
    build essential package content.

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 'use strict';
 
-module.exports = function() {
-  
+module.exports = function(object) {
+  switch(typeof object) {
+    case 'null':
+    case 'undefined':
+      console.log(typeof object);
+      break;
+    case 'function':
+      console.log(buildFunPrintString(object));
+      break;
+    case 'object':
+      console.log(buildObjPrintString(object));
+      break;
+    default:
+      console.log(object.toString());
+      break;
+  }
+}
+
+function buildFunPrintString(object) {
+  return object.toString();
+}
+
+function buildObjPrintString(object) {
+  return object.toString();
 }
