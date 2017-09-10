b352a1205df632abd51782e0db5adc053a14b93c
Jacob Zuo <chopinsky@live.com>
2017-09-10 16:52:28 -0500

    adding more options

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
 'use strict';
 
-module.exports = function(object, noConsoleLog) {
-  var print;
-  switch(typeof object) {
+var defaultOptions = {
+  'logToConsole': false,
+  'excludeNullVals': false,
+  'excludedPropTypes': null
+}
+
+module.exports = function(object, options) {
+  var print, exclude = new Object();
+  var objType = typeof object;
+
+  if (!options) {
+    options = defaultOptions;
+  }
+
+  if (!!options.excludedPropTypes) {
+    var types = options.excludedPropTypes;
+    if (typeof types === 'string') {
+      // if a single string type as the entry
+      exclude[types] = true;
+    } else if (typeof types === 'object' && ~~types.length > 0) {
+      // if a string array of types as the entry
+      types.forEach((t) => {
+        if (typeof t === 'string') {
+          exclude[t] = true;
+        }
+      }, this);
+    }
+  }
+
+  if (exclude[objType]) {
+    return null;
+  }
+
+  switch(objType) {
     case 'null':
     case 'undefined':
-      print = typeof object;
+      if (!options.excludeNullVals) {
+        print = typeof object;
+      }
       break;
     case 'function':
       print = buildFunPrintString(object);
       break;
     case 'object':
-      print = buildObjPrintString(object);
+      print = buildObjPrintString(object, options.excludeNullVals, exclude);
       break;
     default:
       print = object.toString();
       break;
   }
 
-  if (!noConsoleLog) {
+  if (options.logToConsole) {
     console.log(print);
   }
   
@@ -30,19 +63,24 @@ function buildFunPrintString(object) {
   return object.toString();
 }
 
-function buildObjPrintString(object) {
+function buildObjPrintString(object, excludeNullVals, excludedPropTypes) {
   var output = "{\n";
+  var val, valType;
+
   for (var prop in object) {
     if (prop !== null && prop !== undefined) {
-      output += "\t" + prop + " (" + typeof object[prop] + ")"
-      if (typeof object[prop] === 'string' 
-        || typeof object[prop] === 'number'
-        || typeof object[prop] === 'boolean') {
-        output += ": " + object[prop] || "null" + "\n";
-      } else {
-        output += "\n";
+      val = object[prop];
+      valType = typeof val;
+
+      if (excludeNullVals && (val === null || val === undefined)) {
+        continue;
+      } else if (!!excludedPropTypes && excludedPropTypes[valType]) {
+        continue
       }
+
+      output += `  ${prop} (${typeof val}): ${val.toString()}\n`;
     }
   }
+
   return output + "}";
 }
---
+++ spec/test/index.spec.js
@@ -0,0 +1,10 @@
+describe("test string paddings", function() {
+  var peekObject = require('../../index');
+
+  beforeEach(function() {
+  });
+
+  it("required module should not be null", function () {
+    expect(peekObject).not.toBeNull();
+  });
+});
